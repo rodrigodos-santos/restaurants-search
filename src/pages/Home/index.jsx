78fc8e1e3e7afad9e2dfc6554f1b86ae448b6b2f
Rodrigo dos Santos <rodrigo.contato@live.com>
2021-08-15 19:51:57 -0300

    Carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,25 @@
 import React, { useState } from "react"
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon'
+import Slider from "react-slick";
 
 import logo from '../../assets/logo.svg'
-import { Container, Search, Logo, Wrapper, Map } from './styles'
+import restaurante from '../../assets/restaurante-fake.png'
+import { Card } from '../../components'
+
+import { Container, Search, Logo, Wrapper, Map, Carousel, CarouselTitle } from './styles'
 
 const Home = () => {
     const [inputValue, setInputValue] = useState('')
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true
+      }
 
     return (
         <Wrapper>
@@ -22,6 +35,13 @@ const Home = () => {
                         value={inputValue}
                         onChange={(e) => setInputValue(e.target.value)} />
                     </TextField>
+                    <CarouselTitle>Na sua Área</CarouselTitle>
+                    <Carousel {...settings}>
+                        <Card photo={restaurante} title="Nome qualquer"/>
+                        <Card photo={restaurante} title="Nome qualquer"/>
+                        <Card photo={restaurante} title="Nome qualquer"/>
+                        <Card photo={restaurante} title="Nome qualquer"/>
+                    </Carousel>
                 </Search>
             </Container>
             <Map/>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import Slider from "react-slick";
 
 export const Wrapper = styled.div`
     display: flex;
@@ -27,4 +28,18 @@ export const Logo = styled.img`
 export const Map = styled.div`
     background-color: black;
     width: 100%;
+`
+export const Carousel = styled(Slider)`
+    .slick-slide{
+        margin-right: 16px;
+    }
+`
+
+export const CarouselTitle = styled.h1`
+    font-family: ${(props) => props.theme.fonts.regular};
+    color: ${(props) => props.theme.colors.text};
+    font-size: 24px;
+    font-weight: bold;
+    line-height: 29px;
+    margin: 16px 0;
 `
